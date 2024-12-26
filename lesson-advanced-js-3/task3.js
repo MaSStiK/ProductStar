e1eb0dd9edf7f3a0b5504dc63602404a2ea2b6b0
Matvey <72652605+MaSStiK@users.noreply.github.com>
2024-12-26 21:19:18 +0100

    Add files via upload

--- lesson-advanced-js-3/task3.js
+++ lesson-advanced-js-3/task3.js
@@ -4,6 +4,18 @@
 const arr = [10,8,3,2,3,1,2,1,5,6,8,9,4,3,2,4,7,8,6,43,1];
 
 const getAllSubUniqArr = (arr) => {
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        const set = new Set()
+
+        for (let j = i; j < arr.length; j++) {
+            if (set.has(arr[j])) break
+
+            set.add(arr[j])
+            count++
+        }   
+    }
+    return count;
 }
 
 console.log("task3")
