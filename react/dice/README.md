398655c1972b277ca9bfa31af4f513d638c1d7d0
Matvey <72652605+MaSStiK@users.noreply.github.com>
2025-02-20 22:46:47 +0100

    Delete react/dice directory

--- react/dice/README.md
+++
@@ -1,1 +0,0 @@
-![изображение](https://github.com/user-attachments/assets/1514cb7f-1340-4ec7-b9c9-6c2cb928e7c3)
--- react/dice/src/components/Dice/Dice.jsx
+++
@@ -1,9 +0,0 @@
-import "./Dice.css"
-
-export default function Dice(props) {
-    return (
-        <div className="Dice__container">
-            <img src={"/dices/dice" + props.dice + ".png"} alt="dice" />
-        </div>
-    )
-}
