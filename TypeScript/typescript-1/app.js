0324576d9c86a8dd011701035c98000680cf4361
Matvey <72652605+MaSStiK@users.noreply.github.com>
2025-04-13 21:46:35 +0200

    Add files via upload

---
+++ TypeScript/typescript-1/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let p = document.getElementById("text");
+if (p) {
+    p.innerText = "Hello TypeScript!";
+}
---
+++ TypeScript/typescript-1/src/app.ts
@@ -0,0 +1,4 @@
+let p = document.getElementById("text")
+if (p) {
+    p.innerText = "Hello TypeScript!"
+}
