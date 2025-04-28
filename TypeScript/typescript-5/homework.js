1bfae55f3c4efc18cf83dbb49ab10f2e3996553b
Matvey <72652605+MaSStiK@users.noreply.github.com>
2025-04-28 23:25:16 +0200

    Add files via upload

--- TypeScript/typescript-5/homework.js
+++ TypeScript/typescript-5/homework.js
@@ -1,6 +1,6 @@
 "use strict";
 const numbers = [1, 2, 3, 4, 5];
-const target = 3;
+let target = 3;
 function findElement(numbersArray, target) {
     for (let i = 0; i < numbersArray.length; i++) {
         if (numbersArray[i] === target)
@@ -9,8 +9,11 @@ function findElement(numbersArray, target) {
     return -1;
     // Или же можно использовать метод findIndex
 }
-const index = findElement(numbers, target);
+let index = findElement(numbers, target);
 console.log("Задание 1");
+console.log(`Индекс элемента ${target} в массиве: ${index}`);
+target = 5;
+index = findElement(numbers, target);
 console.log(`Индекс элемента ${target} в массиве: ${index}`);
 // ----------
 const person1 = { name: 'Alice', age: 30 };
@@ -33,3 +36,7 @@ function mergeObjects(obj1, obj2) {
 const mergedPerson = mergeObjects(person1, person2);
 console.log("Задание 2");
 console.log(mergedPerson);
+const person3 = { name: 'Alex', city: 'Moscow' };
+const person4 = { age: 20, city: 'Paris' };
+const mergedPerson2 = mergeObjects(person3, person4);
+console.log(mergedPerson2);
--- TypeScript/typescript-5/src/homework.ts
+++ TypeScript/typescript-5/src/homework.ts
@@ -1,5 +1,5 @@
 const numbers = [1, 2, 3, 4, 5];
-const target = 3;
+let target = 3;
 function findElement<T>(numbersArray: T[], target: T): number {
     for (let i = 0; i < numbersArray.length; i++) {
         if (numbersArray[i] === target) return i;
@@ -9,9 +9,14 @@ function findElement<T>(numbersArray: T[], target: T): number {
     // Или же можно использовать метод findIndex
 }
 
-const index = findElement(numbers, target);
+let index = findElement(numbers, target);
 console.log("Задание 1");
 console.log(`Индекс элемента ${target} в массиве: ${index}`);
+
+target = 5;
+index = findElement(numbers, target);
+console.log(`Индекс элемента ${target} в массиве: ${index}`);
+
 
 // ----------
 
@@ -37,3 +42,8 @@ function mergeObjects<T extends object, K extends object>(obj1: T, obj2: K) {
 const mergedPerson = mergeObjects(person1, person2);
 console.log("Задание 2");
 console.log(mergedPerson);
+
+const person3 = { name: 'Alex', city: 'Moscow'};
+const person4 = { age: 20, city: 'Paris' };
+const mergedPerson2 = mergeObjects(person3, person4);
+console.log(mergedPerson2);
