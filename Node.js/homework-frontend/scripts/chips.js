43cbadac3833f86284fcd029f1b222f88b26c6dc
Matvey <72652605+MaSStiK@users.noreply.github.com>
2025-08-10 14:15:16 +0200

    Delete Node.js/homework-frontend directory

--- Node.js/homework-frontend/scripts/chips.js
+++
@@ -1,9 +0,0 @@
-// Активируем контейнер с чипсами, передаем в функцию текст из кнопки
-function bindChips(chipsContainer, func) {
-    $(chipsContainer).find("button").on("click tap", function () {
-        func($(this).text())
-    })
-}
-
-bindChips("#colormagic-chips", getColormagic)
-bindChips("#jikan-chips", getJikan)
