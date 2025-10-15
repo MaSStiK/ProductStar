0b4bc702fe9687e2acd6a00819dfa9a598699120
Matvey Ovchinnkov <72652605+MaSStiK@users.noreply.github.com>
2025-10-15 15:42:41 +0200

    Add files via upload

---
+++ Node.js/nodejs-10/controllers/todoController.js
@@ -0,0 +1,50 @@
+const Todo = require("../models/todo")
+
+module.exports.getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find()
+        res.render("todos", { todos })
+    } catch (error) {
+        console.log("error getTodos", error);
+        res.status(500).send(error)
+    }
+}
+
+module.exports.createTodo = async (req, res) => {
+    const { title, description } = req.body
+    const newTodo = new Todo({ title, description })
+
+    try {
+        await newTodo.save()
+        res.redirect("/todos")
+    } catch (error) {
+        console.log("error createTodo");
+        res.status(500).send(error)
+    }
+}
+
+
+module.exports.updateTodo = async (req, res) => {
+    const { id } = req.params
+    const { title, description, completed } = req.body
+
+    try {
+        await Todo.findByIdAndUpdate(id, { title, description, completed: completed === "on" })
+        res.redirect("/todos")
+    } catch (error) {
+        console.log("error updateTodo");
+        res.status(500).send(error)
+    }
+}
+
+module.exports.deleteTodo = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        await Todo.findByIdAndDelete(id)
+        res.redirect("/todos")
+    } catch (error) {
+        console.log("error deleteTodo");
+        res.status(500).send(error)
+    }
+}
